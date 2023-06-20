f304420116713e800763631d4571fceaf8ae009c
Admin <subashbaidya9@gmail.com>
2023-06-20 20:00:28 +0545

    chore - Tailwind Custom Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5", // text secondary-100
+          200: "#888883", //text secondary-200
+        }
+      }
+    },
   },
   plugins: [],
 }
